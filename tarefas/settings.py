c99060e25bdaef673e1f850c31043922dea7cb17
Murilo Couto <murilo.couto@a7.net.br>
2025-04-08 11:45:39 -0300

    modificaçoes base
    Tipo: M (C-Correção, I-Implementação, M-Melhoria)
    
    Descrição:
    
    Observações:
    
    Referências:

--- tarefas/settings.py
+++ tarefas/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-bh6)y301l+f&jsmmbf_y#nllc93%96z!&--pc&icb=-6gncg7w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
 
 ALLOWED_HOSTS = [
     'localhost',
@@ -81,10 +84,7 @@ WSGI_APPLICATION = 'tarefas.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default='sqlite:///db.sqlite3', conn_max_age=600)
 }
 
 

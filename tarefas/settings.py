07e2106de0c2880630202f5f9af7af383c9ccf1b
Murilo Couto <murilo.couto@a7.net.br>
2025-04-11 15:39:16 -0300

    Atualização
    Tipo: M (C-Correção, I-Implementação, M-Melhoria)
    
    Descrição:
    
    Observações:
    
    Referências:

--- tarefas/settings.py
+++ tarefas/settings.py
@@ -63,7 +63,10 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 ROOT_URLCONF = 'tarefas.urls'
 
